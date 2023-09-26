879b68dbee1940ed18517a7d2572d8ef75b72072
jo-project <jo.project.0911@gmail.com>
2023-09-27 00:54:26 +1000

    Bot

--- lua/jovim/setup/lualinealt/components/fancy_branch.lua
+++ lua/jovim/setup/lualinealt/components/fancy_branch.lua
@@ -1,7 +1,7 @@
 local M = require("lualine.components.branch"):extend()
 
 function M:init(options)
-    options.icon = options.icon or { "" }-- color = { fg = "#fc5603" } }
+    options.icon = options.icon or { "", color = { fg = "#a6e3a1"} }-- color = { fg = "#fc5603" } }
     M.super.init(self, options)
 end
 
