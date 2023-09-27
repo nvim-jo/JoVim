af5bf4a941ba57ab0635acfa5f89b889edfb9823
jo-project <jo.project.0911@gmail.com>
2023-09-27 22:58:37 +1000

    JoVim

--- lua/jovim/setup/catppuccin/colors.lua
+++ lua/jovim/setup/catppuccin/colors.lua
@@ -152,6 +152,7 @@ M.overrides = {
 			NeoTreeTabActive = { bg = my_palette.mantle, fg = my_palette.lavender, style = { "bold" } },
 			NeoTreeTabInactive = { bg = my_palette.base, fg = my_palette.overlay0 },
 
+			MiniIndentscopeSymbol = { fg = my_palette.blue },
 			MatchParen = { fg = my_palette.blue, bg = my_palette.surface0, style = { "bold" } },
 			ColorColumn = { bg = my_palette.crust }, -- used for the columns set with 'colorcolumn'	
 		},
