7764afd65366a919e23f5c75544fc720852294ca
jo-project <jo.project.0911@gmail.com>
2023-09-29 16:53:09 +1000

    JoVim

--- lua/jovim/setup/indent-blank.lua
+++ lua/jovim/setup/indent-blank.lua
@@ -1,11 +1,11 @@
 local highlight = {
-    "RainbowRed",
-    "RainbowYellow",
     "RainbowBlue",
-    "RainbowOrange",
+    "RainbowMauve",
+    "RainbowLavender",
+    "RainbowRed",
     "RainbowGreen",
-    "RainbowViolet",
-    "RainbowCyan",
+    "RainbowYellow",
+    "RainbowRosewater",
 }
 
 local hooks = require "ibl.hooks"
@@ -13,20 +13,19 @@ local hooks = require "ibl.hooks"
 local M = {}
 M.setup = function ()
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#89B4FA" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#CBA6F7" })
+        vim.api.nvim_set_hl(0, "RainbowLavender", { fg = "#B4BEFE" })
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#F38BA8" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#A6E3A1" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#F9E2AF" })
+        vim.api.nvim_set_hl(0, "RainbowRosewater", { fg = "#F5E0DC" })
     end)
 
     
     vim.g.rainbow_delimiters = { highlight = highlight }
     require("ibl").setup { 
         scope = { highlight = highlight },
-        indent = { highlight = highlight },
         char = "│",
         filetype_exclude = {
             "help",
