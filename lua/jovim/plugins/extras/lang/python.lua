9f388c1a2b7f8b60c420fffd0520e065aac2df2d
jo-project <jo.project.0911@gmail.com>
2023-09-30 20:53:44 +1000

    JoVim

--- lua/jovim/plugins/extras/lang/python.lua
+++ lua/jovim/plugins/extras/lang/python.lua
@@ -12,19 +12,12 @@ return {
     opts = {
       servers = {
         pyright = {
-          capabilities = (function()
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
-            return capabilities
-          end)(),
-          settings = {
-            python = {
-              analysis = {
-                useLibraryCodeForTypes = true,
-                diagnosticSeverityOverrides = {
-                  reportUnusedVariable = "warning", -- or anything
+          capabilities = {
+            textDocument = {
+              publishDiagnostics = {
+                tagSupport = {
+                  valueSet = { 2 },
                 },
-                typeCheckingMode = "basic",
               },
             },
           },
