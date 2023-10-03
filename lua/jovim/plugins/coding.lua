daef4a4c533321d52c7a47412f1df3005d6e54da
jo-project <jo.project.0911@gmail.com>
2023-10-04 09:39:09 +1100

    JoVim

--- lua/jovim/plugins/coding.lua
+++ lua/jovim/plugins/coding.lua
@@ -264,7 +264,6 @@ return {
     "linux-cultist/venv-selector.nvim",
     cmd = {"VenvSelect", "VenvSelectCached"},
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
     keys = {{
       -- Keymap to open VenvSelector to pick a venv.
       "<leader>vs", "<cmd>:VenvSelect<cr>",
