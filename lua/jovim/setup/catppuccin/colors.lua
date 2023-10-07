34bbaaf765f0dc63248529a14f9a39c147751cc2
jo-project <jo.project.0911@gmail.com>
2023-10-07 23:38:28 +1100

    JoVim

--- lua/jovim/setup/catppuccin/colors.lua
+++ lua/jovim/setup/catppuccin/colors.lua
@@ -43,132 +43,131 @@ function M.get_palette()
 	return my_palette
 end
 
+local default_overrides = {
+	MarkingWindow = { fg = my_palette.text },
+	MarkingBorder = { fg = my_palette.blue },
+
+	CursorLine = { bg = my_palette.mantle },
+	CursorColumn = { bg = my_palette.mantle },
+	Visual = { bg = my_palette.mantle },
+
+	FloatBorder = {
+		fg = my_palette.blue,
+		bg = my_palette.none,
+	},
+
+	NormalFloat = {
+		bg = my_palette.none
+	},
+
+	FloatTitle = {
+		fg = my_palette.text
+	},
+
+	-- For native lsp configs
+	DiagnosticVirtualTextError = { bg = my_palette.none },
+	DiagnosticVirtualTextWarn = { bg = my_palette.none },
+	DiagnosticVirtualTextInfo = { bg = my_palette.none },
+	DiagnosticVirtualTextHint = { bg = my_palette.none },
+	LspInfoBorder = { link = "FloatBorder" },
+
+	-- For indent-blankline
+	IndentBlanklineChar = { fg = utils.darken(my_palette.surface0, 0.40, my_palette.mantle), style = {} },
+	IndentBlanklineContextChar = { fg = my_palette.lavender, style = { "bold" } },
+	IndentBlanklineContextStart = { sp = my_palette.lavender, style = { "underline" } },
+
+	-- For nvim-cmp and wilder.nvim
+	CmpItemAbbrMatch = { fg = my_palette.blue },
+	CmpItemAbbrMatchFuzzy = { fg = my_palette.blue },
+	CmpDoc = { link = "NormalFloat" },
+	CmpDocBorder = {
+		fg = my_palette.blue,
+		bg = my_palette.none,
+	},
+	CmpBorder = {
+		fg = my_palette.blue,
+		bg = my_palette.none,
+	},
+	Pmenu = { bg = my_palette.none },
+	PmenuSel = { fg = my_palette.sapphire, bg = my_palette.none },
+	PmenuSbar = { bg = my_palette.none },  -- Popup menu: scrollbar.
+	PmenuThumb = { bg = my_palette.mauve }, -- Popup menu: Thumb of the scrollbar.
+
+	VertSplit = { fg = my_palette.surface0 },
+	NonText = { fg = my_palette.surface0 },
+	HlSearchLensNear = { fg = my_palette.sky, bg = mocha.surface0 },
+	HlSearchLens = { fg = my_palette.sky, bg = mocha.surface1 },
+	GitSignsAdd = { fg = my_palette.diff.add },
+	GitSignsChange = { fg = my_palette.diff.change },
+	GitSignsDelete = { fg = my_palette.diff.delete },
+
+	TabLineSel = { bg = my_palette.red },
+
+	-- telescope
+	-- For telescope.nvim
+	TelescopeBorder = {
+		fg = my_palette.blue,
+		bg = my_palette.none,
+	},
+	TelescopePromptBorder = {
+		fg = my_palette.blue,
+		bg = my_palette.none,
+	},
+	TelescopePromptNormal = {
+		fg = my_palette.text,
+		bg = my_palette.none,
+	},
+	TelescopePromptPrefix = {
+		fg = my_palette.flamingo,
+		bg = my_palette.none,
+	},
+	TelescopeNormal = {
+		bg = my_palette.none,
+	},
+	TelescopePreviewTitle = {
+		fg = my_palette.green,
+		bg = my_palette.none,
+	},
+	TelescopePromptTitle = {
+		fg = my_palette.red,
+		bg = my_palette.none,
+	},
+	TelescopeResultsTitle = {
+		fg = my_palette.mantle,
+		bg = my_palette.none,
+	},
+	TelescopeSelection = {
+		fg = my_palette.green,
+		bg = my_palette.none,
+	},
+	TelescopeResultsDiffAdd = { fg = my_palette.green },
+	TelescopeResultsDiffChange = { fg = my_palette.yellow },
+	TelescopeResultsDiffDelete = { fg = my_palette.red },
+
+	["@keyword.return"] = { fg = my_palette.pink, style = {} },
+
+	MiniIndentscopeSymbol = { fg = my_palette.blue },
+	IlluminatedWordText = { bg = utils.darken(my_palette.surface0, 0.3, my_palette.mantle) },
+	IlluminatedWordRead = { bg = utils.darken(my_palette.surface0, 0.3, my_palette.mantle) },
+	IlluminatedWordWrite = { bg = utils.darken(my_palette.surface0, 0.3, my_palette.mantle) },
+
+	NoiceCmdlineIcon = { fg = my_palette.mauve },
+	NoiceCmdlinePopupBorder = { fg = my_palette.blue },
+	NoiceCmdlinePopupTitle = { fg = my_palette.text },
+
+	MatchParen = { fg = my_palette.blue, bg = utils.darken(my_palette.surface0, 0.3, my_palette.mantle), style = { "bold" } },
+}
+
+local jotree_highlight = require('jovim.setup.catppuccin.highlights.jotree')
+
+local highlight_overrides = vim.tbl_deep_extend("force", default_overrides, jotree_highlight)
+
 M.overrides = {
 	colors = {
 		all = mocha_overrides,
 	},
 	highlights = {
-		all = {
-			MarkingWindow = { fg = my_palette.text },
-			MarkingBorder = { fg = my_palette.blue },
-
-			CursorLine = { bg = my_palette.mantle },
-			CursorColumn = { bg = my_palette.mantle },
-			Visual = { bg = my_palette.mantle },
-
-			FloatBorder = {
-				fg = my_palette.blue,
-				bg = my_palette.none,
-			},
-
-			NormalFloat = {
-				bg = my_palette.none
-			},
-
-			FloatTitle = {
-				fg = my_palette.text
-			},
-
-			-- For native lsp configs
-			DiagnosticVirtualTextError = { bg = my_palette.none },
-			DiagnosticVirtualTextWarn = { bg = my_palette.none },
-			DiagnosticVirtualTextInfo = { bg = my_palette.none },
-			DiagnosticVirtualTextHint = { bg = my_palette.none },
-			LspInfoBorder = { link = "FloatBorder" },
-
-			-- For indent-blankline
-			IndentBlanklineChar = { fg = utils.darken(my_palette.surface0, 0.40, my_palette.mantle), style = {} },
-			IndentBlanklineContextChar = { fg = my_palette.lavender, style = { "bold" } },
-			IndentBlanklineContextStart = { sp = my_palette.lavender, style = { "underline" } },
-
-			-- For nvim-cmp and wilder.nvim
-			CmpItemAbbrMatch = { fg = my_palette.blue },
-			CmpItemAbbrMatchFuzzy = { fg = my_palette.blue },
-			CmpDoc = { link = "NormalFloat" },
-			CmpDocBorder = {
-				fg = my_palette.blue,
-				bg = my_palette.none,
-			},
-			CmpBorder = {
-				fg = my_palette.blue,
-				bg = my_palette.none,
-			},
-			Pmenu = { bg = my_palette.none },
-			PmenuSel = { fg = my_palette.sapphire, bg = my_palette.none },
-			PmenuSbar = { bg = my_palette.none }, -- Popup menu: scrollbar.
-			PmenuThumb = { bg = my_palette.mauve }, -- Popup menu: Thumb of the scrollbar.
-
-			VertSplit = { fg = my_palette.surface0 },
-			NonText = { fg = my_palette.surface0 },
-			HlSearchLensNear = { fg = my_palette.sky, bg = mocha.surface0 },
-			HlSearchLens = { fg = my_palette.sky, bg = mocha.surface1 },
-			GitSignsAdd = { fg = my_palette.diff.add },
-			GitSignsChange = { fg = my_palette.diff.change },
-			GitSignsDelete = { fg = my_palette.diff.delete },
-
-			TabLineSel = { bg = my_palette.red },
-
-			-- telescope
-			-- For telescope.nvim
-			TelescopeBorder = {
-				fg = my_palette.blue,
-				bg = my_palette.none,
-			},
-			TelescopePromptBorder = {
-				fg = my_palette.blue,
-				bg = my_palette.none,
-			},
-			TelescopePromptNormal = {
-				fg = my_palette.text,
-				bg = my_palette.none,
-			},
-			TelescopePromptPrefix = {
-				fg = my_palette.flamingo,
-				bg = my_palette.none,
-			},
-			TelescopeNormal = {
-				bg = my_palette.none,
-			},
-			TelescopePreviewTitle = {
-				fg = my_palette.green,
-				bg = my_palette.none,
-			},
-			TelescopePromptTitle = {
-				fg = my_palette.red,
-				bg = my_palette.none,
-			},
-			TelescopeResultsTitle = {
-				fg = my_palette.mantle,
-				bg = my_palette.none,
-			},
-			TelescopeSelection = {
-				fg = my_palette.green,
-				bg = my_palette.none,
-			},
-			TelescopeResultsDiffAdd = { fg = my_palette.green },
-			TelescopeResultsDiffChange = { fg = my_palette.yellow },
-			TelescopeResultsDiffDelete = { fg = my_palette.red },
-
-			["@keyword.return"] = { fg = my_palette.pink, style = {} },
-
-			JoTreeRootName = { fg = my_palette.lavender, style = { "bold" } },
-			JoTreeNormal = { fg = my_palette.text, bg = my_palette.none },
-			JoTreeNormalNC = { fg = my_palette.text, bg = my_palette.mantle },
-			-- JoTreeTabActive = { bg = my_palette.mantle, fg = my_palette.lavender, style = { "bold" } },
-			-- JoTreeTabInactive = { bg = my_palette.base, fg = my_palette.overlay0 },
-			JoTreeIndentMarker = { fg = my_palette.blue },
-
-			MiniIndentscopeSymbol = { fg = my_palette.blue },
-			IlluminatedWordText = { bg = utils.darken(my_palette.surface0, 0.3, my_palette.mantle) },
-			IlluminatedWordRead = { bg = utils.darken(my_palette.surface0, 0.3, my_palette.mantle) },
-			IlluminatedWordWrite = { bg = utils.darken(my_palette.surface0, 0.3, my_palette.mantle) },
-
-			NoiceCmdlineIcon = { fg = my_palette.mauve },
-			NoiceCmdlinePopupBorder = { fg = my_palette.blue },
-			NoiceCmdlinePopupTitle = { fg = my_palette.text },
-
-			MatchParen = { fg = my_palette.blue, bg = utils.darken(my_palette.surface0, 0.3, my_palette.mantle), style = { "bold" } },
-		},
+		all = highlight_overrides
 	},
 }
 
@@ -256,7 +255,6 @@ function M.get_options()
 				dim_context = false,
 				alt_background = false,
 			},
-			-- Jotree = true,
 		},
 		color_overrides = M.overrides.colors,
 		highlight_overrides = M.overrides.highlights,
---
+++ lua/jovim/setup/catppuccin/highlights/jotree.lua
@@ -0,0 +1,40 @@
+local C = require('jovim.setup.catppuccin.colors').get_palette()
+
+local active_bg = C.mantle
+local inactive_bg = C.base
+
+return {
+    JoTreeDirectoryName = { fg = C.blue },
+    JoTreeDirectoryIcon = { fg = C.blue },
+    JoTreeExpander = { fg = C.overlay0 },
+    JoTreeSymbolicLinkTarget = { fg = C.pink },
+
+    JoTreeGitAdded = { fg = C.green },
+    JoTreeGitConflict = { fg = C.red },
+    JoTreeGitDeleted = { fg = C.red },
+    JoTreeGitIgnored = { fg = C.overlay0 },
+    JoTreeGitModified = { fg = C.yellow },
+    JoTreeGitUnstaged = { fg = C.red },
+    JoTreeGitUntracked = { fg = C.blue },
+    JoTreeGitStaged = { fg = C.green },
+
+    JoTreeFloatBorder = { link = "FloatBorder" },
+    JoTreeFloatTitle = { link = "FloatTitle" },
+
+    JoTreeFileNameOpened = { fg = C.pink },
+    JoTreeDimText = { fg = C.overlay1 },
+    JoTreeFilterTerm = { fg = C.green, style = { "bold" } },
+    JoTreeTabActive = { bg = active_bg, fg = C.lavender, style = { "bold" } },
+    JoTreeTabInactive = { bg = inactive_bg, fg = C.overlay0 },
+    JoTreeTabSeparatorActive = { fg = active_bg, bg = active_bg },
+    JoTreeTabSeparatorInactive = { fg = inactive_bg, bg = inactive_bg },
+    JoTreeVertSplit = { fg = C.base, bg = inactive_bg },
+    JoTreeStatusLineNC = { fg = C.mantle, bg = C.mantle },
+
+    JoTreeRootName = { fg = C.lavender, style = { "bold" } },
+    JoTreeNormal = { fg = C.text, bg = C.none },
+    JoTreeNormalNC = { fg = C.text, bg = C.mantle },
+    -- JoTreeTabActive = { bg = C.mantle, fg = C.lavender, style = { "bold" } },
+    -- JoTreeTabInactive = { bg = C.base, fg = C.overlay0 },
+    JoTreeIndentMarker = { fg = C.blue },
+}
