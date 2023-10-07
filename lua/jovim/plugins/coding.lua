7af7ea28732b2345cd045ee5871dd0377889e312
jo-project <jo.project.0911@gmail.com>
2023-10-07 23:00:39 +1100

    JoVim

--- lua/jovim/plugins/coding.lua
+++ lua/jovim/plugins/coding.lua
@@ -39,6 +39,7 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lua",
       "saadparwaiz1/cmp_luasnip",
     },
     opts = function()
