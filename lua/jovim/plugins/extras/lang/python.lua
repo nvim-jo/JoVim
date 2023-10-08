43c1f72a533f7e40f6166861ae5d5f6c473f5179
jo-project <jo.project.0911@gmail.com>
2023-10-08 23:22:03 +1100

    JoVim

--- lua/jovim/plugins/extras/lang/python.lua
+++ lua/jovim/plugins/extras/lang/python.lua
@@ -12,25 +12,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        pyright = {
-          -- capabilities = (function ()
-          --   local capabilities = vim.lsp.protocol.make_client_capabilities()
-          --   capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
-          -- end)(),
-          capabilities = capabilities,
-          settings = {
-            python = {
-              analysis = {
-                useLibraryCodeForTypes = true,
-                diagnosticSeverityOverrides = {
-                  reportUnusedVariable = "warning"
-                },
-                -- diagnosticMode = 'basic',
-                typeCheckingMode = "off",
-              }
-            }
-          }
-        }, 
+        pyright = {}, 
         ruff_lsp = {},
       }, 
       setup = {
