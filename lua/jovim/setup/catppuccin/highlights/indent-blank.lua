e675e20ccd15b88542e17d251a4ae0a99e1db596
jo-project <jo.project.0911@gmail.com>
2023-10-10 08:02:33 +1100

    JoVim

--- lua/jovim/setup/catppuccin/highlights/indent-blank.lua
+++ lua/jovim/setup/catppuccin/highlights/indent-blank.lua
@@ -3,7 +3,7 @@ local utils = require('catppuccin.utils.colors')
 
 return {
     -- For indent-blankline
-    IndentBlanklineChar = { fg = utils.darken(C.surface0, 0.40, C.mantle), style = {} },
+    IndentBlanklineChar = { fg = C.surface0, style = {} },
     IndentBlanklineContextChar = { fg = C.lavender, style = { "bold" } },
     IndentBlanklineContextStart = { sp = C.lavender, style = { "underline" } },
 }
