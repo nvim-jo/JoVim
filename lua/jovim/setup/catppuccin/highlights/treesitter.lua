d8f0273f9f53756ed183b33015eb909a203e7b55
jo-project <jo.project.0911@gmail.com>
2023-10-10 12:34:19 +1100

    JoVim

--- lua/jovim/setup/catppuccin/highlights/treesitter.lua
+++ lua/jovim/setup/catppuccin/highlights/treesitter.lua
@@ -15,5 +15,5 @@ return {
 	["@type.builtin"] = { fg = C.peach },
 	["@pyliteral"] = { fg = C.mauve, style = {} },
 	["@namespace"] = { fg = C.text },
-	["@include"] = { fg = C.text },
+	["@storageclass"] = { fg = C.text },
 }
