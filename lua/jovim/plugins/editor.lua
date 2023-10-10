71034f48a0e8fc6d6a9052e9275140003c293c0b
jo-project <jo.project.0911@gmail.com>
2023-10-10 14:10:58 +1100

    JoVim

--- lua/jovim/plugins/editor.lua
+++ lua/jovim/plugins/editor.lua
@@ -307,7 +307,7 @@ return {
     config = function()
       -- calling `setup` is optional for customization
       require("fzf-lua").setup({
-        "fzf-vim",
+        "default",
         winopts = {
           border           = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
           preview = {
