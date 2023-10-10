57218d88166b052c38b4e206ab1b7f20f4de20a1
jo-project <jo.project.0911@gmail.com>
2023-10-10 13:42:24 +1100

    JoVim

--- lua/jovim/plugins/editor.lua
+++ lua/jovim/plugins/editor.lua
@@ -302,10 +302,11 @@ return {
 
   {
     "ibhagwan/fzf-lua",
+    cmd = "FzfLua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       -- calling `setup` is optional for customization
-      require("fzf-lua").setup({})
+      require("fzf-lua").setup({"telescope",winopts={preview={default="bat"}}})
     end
   },
 
