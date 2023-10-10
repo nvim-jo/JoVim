700dabbbccc0215f59eefd239999090f219839b9
jo-project <jo.project.0911@gmail.com>
2023-10-10 17:30:43 +1100

    JoVim

--- lua/jovim/plugins/editor.lua
+++ lua/jovim/plugins/editor.lua
@@ -219,6 +219,7 @@ return {
 
       return {
         defaults = {
+          winblend = 0,
           file_ignore_patterns = { "node%_modules/.*", "venv/.*" },
           prompt_prefix = " ",
           selection_caret = " ",
@@ -237,13 +238,30 @@ return {
           end,
           layout_strategy = "horizontal",
           layout_config = {
+            prompt_position = "top",
             horizontal = {
-              prompt_position = "top",
-              preview_width = 0.5,
+              preview_width = function(_, cols, _)
+                if cols > 200 then
+                  return math.floor(cols * 0.4)
+                else
+                  return math.floor(cols * 0.6)
+                end
+              end,
             },
-            width = 0.8,
-            height = 0.8,
-            preview_cutoff = 120,
+            vertical = {
+              width = 0.9,
+              height = 0.95,
+              preview_height = 0.5,
+            },
+      
+            flex = {
+              horizontal = {
+                preview_width = 0.9,
+              },
+            },
+            width = 0.95,
+            height = 0.85,
+            -- preview_cutoff = 120,
           },
           sorting_strategy = "ascending",
           mappings = {
