c06d4aa0e130bd3bf8938d936a4da2b9b71c927f
jo-project <jo.project.0911@gmail.com>
2023-10-10 16:42:48 +1100

    JoVim

--- lua/jovim/plugins/extras/lang/python.lua
+++ lua/jovim/plugins/extras/lang/python.lua
@@ -48,14 +48,8 @@ return {
   },
   {
 		"stevearc/conform.nvim",
+    ft = { "python" },
 		event = "BufWritePre", -- load the plugin before saving,
-    cond = function()
-        local ft = vim.bo.filetype
-        if ft == "python" then
-          return true
-        end
-        return false
-    end,
 		opts = {
 			formatters_by_ft = {
 				-- first use isort and then black
