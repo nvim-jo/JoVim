4a189b744e562516311f321aab5eb3f0bfff7a06
jo-project <jo.project.0911@gmail.com>
2023-10-11 17:56:09 +1100

    JoVim

--- lua/jovim/config/keymaps.lua
+++ lua/jovim/config/keymaps.lua
@@ -29,8 +29,8 @@ map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window wi
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- Move Lines
-map("n", "^]j", "<cmd>m .+1<cr>==", { desc = "Move down" })
-map("n", "^]k", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("n", "<C-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<C-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
 map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
 map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
 map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
