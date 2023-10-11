d06df00cd7c4feaaee7d66a676a852358f845298
jo-project <jo.project.0911@gmail.com>
2023-10-12 06:15:08 +1100

    JoVim

--- lua/jovim/plugins/extras/lang/python.lua
+++ lua/jovim/plugins/extras/lang/python.lua
@@ -103,4 +103,19 @@ return {
   {
     "nvim-jo/selenv.nvim",
   },
+  {
+    "linux-cultist/venv-selector.nvim",
+    cmd = { "VenvSelect" },
+    opts = {
+      -- Your options go here
+      name = "venv",
+      auto_refresh = true
+    },
+    keys = {{
+      -- Keymap to open VenvSelector to pick a venv.
+      "<leader>vs", "<cmd>:VenvSelect<cr>",
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      "<leader>vc", "<cmd>:VenvSelectCached<cr>"
+    }}
+  }
 }
