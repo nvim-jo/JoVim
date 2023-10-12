5e0e19cbb86453127c6697bb8a8a054229290166
jo-project <jo.project.0911@gmail.com>
2023-10-12 22:27:48 +1100

    JoVim

--- lua/jovim/plugins/init.lua
+++ lua/jovim/plugins/init.lua
@@ -1,3 +1,13 @@
+if vim.fn.has("nvim-0.9.0") == 0 then
+  vim.api.nvim_echo({
+    { "JoVim requires Neovim >= 0.9.0\n", "ErrorMsg" },
+    { "Press any key to exit", "MoreMsg" },
+  }, true, {})
+  vim.fn.getchar()
+  vim.cmd([[quit]])
+  return {}
+end
+
 require("jovim.config").init()
 
 return {
